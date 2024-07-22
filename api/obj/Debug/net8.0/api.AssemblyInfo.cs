36d6a40b929303e2f8e2f13225ace88c52267af6
Danieljohansen97 <danieljohansen1997@hotmail.com>
2024-07-22 18:21:19 +0200

    Implement comments

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b91b9b9ceb56efdc6eb2774c0c6a9c8c8f96db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d5b644e93f9d3ddb4d5d49ac95e1141bb8cab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
