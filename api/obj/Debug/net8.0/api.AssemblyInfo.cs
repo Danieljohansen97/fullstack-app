7d1f191c2e2a2ded3329816eba488ad2dd70cdc8
Danieljohansen97 <danieljohansen1997@hotmail.com>
2024-07-22 21:54:06 +0200

    Implement Delete Comment

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f70884d533ab8802515d7680008f6759eb87c3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7eb061cae3ea3869069dca816fb3faf061f2368")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
