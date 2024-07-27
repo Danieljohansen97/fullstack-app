6fe457611ea95cdd1d6037e396a2a718a3078b70
Danieljohansen97 <danieljohansen1997@hotmail.com>
2024-07-27 15:04:39 +0200

    Add authorization and lock down /api/stock

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d4a0801ced25437cd2a18df96a2b02d5899462")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfad485537cff1fc9ae63b94c3cc414940804706")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
