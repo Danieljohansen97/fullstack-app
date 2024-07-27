d345fcb3d0196c3f563ba88a6f6da0d0e10eb68b
Danieljohansen97 <danieljohansen1997@hotmail.com>
2024-07-27 20:09:48 +0200

    Add simple Search component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import './App.css';
 
 // Components
 import CardList from './Components/CardList/CardList';
+import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
