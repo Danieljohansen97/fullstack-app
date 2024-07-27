8b46e30e4a62974c65af859469a9799f80136165
Danieljohansen97 <danieljohansen1997@hotmail.com>
2024-07-27 23:03:15 +0200

    Type narrowing and getting data from external API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,33 @@
 import { ChangeEvent, SyntheticEvent, useState } from "react";
 import "./App.css";
 
-// Components
 import CardList from "./Components/CardList/CardList";
 import Search from "./Components/Search/Search";
+import { CompanySearch } from "./company";
+import { searchCompanies } from "./api";
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
   };
 
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if(result !== undefined &&Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    } else {
+      // Handle unexpected return type
+      // This is a followup issue due to isAxiosError not working.
+      console.log("Unexpected return type from searchCompanies: ", result);
+    }
+    console.log(searchResult)
   };
 
   return (
